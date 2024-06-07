55ce29eef8f3cc0b06e42f29e9137b86f1693e27
ShantanuKH <khadseshantanu02@gmail.com>
2024-06-07 16:55:11 +0530

    Import assests, Convert glb to jsx, Used in Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,8 +2,28 @@
 import {React, Suspense } from 'react';
 import { Canvas } from '@react-three/fiber';
 import Loader from '../components/Loader';
+import Island from '../models/Island';
 
 const Home = () => {
+  
+  const adjustIslandForScreenSize = () => {
+    let screenScale = null;
+    let screenPosition = [0, -6.5, -43];
+    let rotation = [0.1, 4.7, 0]
+    if(window.innerWidth < 768){
+      screenScale = [0.9, 0.9, 0.9];
+     
+    }else{
+      screenScale = [1, 1, 1];
+    }
+
+    return[screenScale, screenPosition, rotation]
+  }
+
+  const [islandScale, islandPosition, islandRotation] = adjustIslandForScreenSize();
+
+
+
   return (
     <section className='w-full h-screen relative'>
       {/* <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
@@ -12,6 +32,18 @@ const Home = () => {
       <Canvas className='w-full h-full bg-transperent ' camera={{near: 0.1, far: 1000}}>
 
         <Suspense fallback={<Loader/>}>
+          <directionalLight/>
+          <ambientLight/>
+          <pointLight/>
+          <spotLight/>
+          <hemisphereLight/>
+
+          <Island
+            position = {islandPosition}
+            scale = {islandScale}
+            rotation = {islandRotation}
+          
+          />
 
         </Suspense>
 
